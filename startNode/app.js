98477e91c5986c2b01032ae21cba0070a9756212
Milind <44804793+MilindDev17@users.noreply.github.com>
2018-11-09 07:35:12 +0530

    starting NOde js

---
+++ startNode/app.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const os = require('os');
+const notes = require('./notes');
+
+const _ = require('lodsh');
+const yargs = require('yargs');
+
+const argv = yargs.options({
+	help = 	 
+
+})  
+
+//console.log(notes.add(9,2));
+
+if(command == 'add'){
+	console.log('hi');
+}
+
+//var users = os.userInfo();
+// fs.appendFile('textCretefs.txt',`Hello test first one ${users.username}`);
